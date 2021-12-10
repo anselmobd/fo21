fd072cd6637b4dac3d6195ccce5d99747d649976
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-12-10 13:12:42 -0300

    alterando forma de guardar SECRET_KEY

--- src/fo21/settings.py
+++ src/fo21/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+from .secrets import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,8 +24,6 @@ ROOT_DIR = os.path.dirname(BASE_DIR)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-9#8^!5v*l!p_a0m$vf_rg+ii3z7i-t+d7te@7444esik=uzx6o'
-with open(os.path.join(ROOT_DIR, 'etc/secret_key.txt')) as f:
-    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
